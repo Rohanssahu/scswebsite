eb195199c2749f598dee9c4c1d36fc4b50d3b1e0
Rohan Sahu <rohansahusahi@gmail.com>
2026-03-14 14:30:01 +0530

    h

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: "/scswebsite/",   // ⭐ IMPORTANT
+export default defineConfig({
+  base: "/",   // ⭐ custom domain ke liye
 
-  server: {
-    host: "::",
-    port: 8080,
-  },
-
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
